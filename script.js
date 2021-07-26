506a597a54b4c0db319806565b3b132ac11a6f1a
rpire <rubenpire7@gmail.com>
2021-07-26 15:59:35 -0400

    Add functionality and decorations to the toggle menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
   const burger = document.querySelector("#burger-container");
   const menu = document.querySelector("#menu");
+  const cross = document.querySelector("#cross");
   burger.classList.toggle("invisible");
   menu.classList.toggle("js-nav");
+  cross.classList.toggle("invisible");
 }
