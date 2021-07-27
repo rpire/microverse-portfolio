88e9776466ee16c4a8905ca871a69e5a4344f9fe
Reshy <reshy@protonmail.com>
2021-07-27 15:10:28 +0200

    Add dynamic modal content

--- modal.js
+++ modal.js
@@ -7,7 +7,8 @@ const card1 = {
       'Ruby on Rails',
       'JavaScript'
     ],
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'http://www.github.com'
   };
 
   const card2 = {
@@ -17,7 +18,8 @@ const card1 = {
       'Ruby on Rails',
       'JavaScript'
     ],
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'http://www.google.com'
   };
 
   const card3 = {
@@ -27,7 +29,8 @@ const card1 = {
       'Ruby on Rails',
       'JavaScript'
     ],
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'https://github.com/rpire/microverse-portfolio/tree/modal'
   };
 
   const projects = [card1, card2, card3];
@@ -35,11 +38,22 @@ const card1 = {
   function modal(number){
     let title = document.querySelector("#card-title");
     let list = document.querySelector("#card-list");
+    list.innerHTML = '';
     let img = document.querySelector(".card-image");
     let desc = document.querySelector("#card-description");
+    let buttonsrc = document.querySelector("#card-button-source");
+    let buttonlive = document.querySelector("#card-button-live");
 
-    console.log(cardImages);
     title.innerHTML = projects[number].title;
+
+    for (let i=0;i<projects[number].technologies.length;i++) {
+        let li = document.createElement('li');
+	      li.textContent = projects[number].technologies[i];
+	      list.appendChild(li);
+      }
+
     img.setAttribute("src", cardImages+(number+1)+".svg");
+    buttonsrc.setAttribute("onclick", "window.open('"+projects[number].link+"', '_blank');");
+    buttonlive.setAttribute("onclick", "window.open('"+projects[number].link+"', '_blank');");
     desc.innerHTML = projects[number].description;
 };
