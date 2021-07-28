d846d3b16f99326a5425146285f4de604d1f985d
rpire <rubenpire7@gmail.com>
2021-07-28 10:20:11 -0400

    Apply styling to the error message

--- form-validation.js
+++ form-validation.js
@@ -2,13 +2,14 @@ function validation() {
   const form = document.getElementById('form');
   const email = form.elements['email'].value;
   const message = document.getElementById('message');
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  message.innerHTML = "";
+  const re = /@/g;
   if (re.test(String(email).toLowerCase()) == true) {
     if (email !== email.toLowerCase()) {
       form.addEventListener('submit', function(event) {
         event.preventDefault();
       });
-      message.innerHTML = "Make sure your email is in lower case.";
+      message.innerHTML = "* Make sure your email is in lower case.";
       console.log('invalid email!');
     } else {
       form.submit();
