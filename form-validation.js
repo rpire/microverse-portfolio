6173caa291051c5a5ba01fd8887fbf0793645740
rpire <rubenpire7@gmail.com>
2021-07-28 13:41:48 -0400

    Implement requested changes

--- form-validation.js
+++ form-validation.js
@@ -5,26 +5,24 @@ function validation() {
   const comment = form.elements.comment.value;
   const message = document.getElementById('message');
   message.innerHTML = '';
-  const re = /@/g;
-  if (re.test(String(email).toLowerCase()) === true) {
+  console.log(form.elements.name.value)
+  form.addEventListener('submit', (event) => {
+    const form = document.getElementById('form');
     if (name.length < 1) {
-      form.addaddEventListener('submit', (event) => {
-        event.preventDefault();
-      });
+      event.preventDefault();
       message.innerHTML = '* Make sure to fill your name';
     } else if (email !== email.toLowerCase()) {
-      form.addEventListener('submit', (event) => {
-        event.preventDefault();
-      });
+      event.preventDefault();
       message.innerHTML = '* Make sure your email is in lower case.';
     } else if (comment.length < 5) {
-      form.addEventListener('submit', (event) => {
-        event.preventDefault();
-      });
+      event.preventDefault();
       message.innerHTML = '* Text area should have at least 5 characters';
     } else {
       form.submit();
     }
-  }
+  })
 }
-validation();
+
+if (1>2) {
+  validation();
+}
