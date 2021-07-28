19f5a441dc5c119d90fd9e0d616863ec90eecd16
rpire <rubenpire7@gmail.com>
2021-07-28 11:58:48 -0400

    Add new requirements and error messages

--- form-validation.js
+++ form-validation.js
@@ -1,16 +1,31 @@
 function validation() {
   const form = document.getElementById('form');
+  const name = form.elements.name.value;
   const email = form.elements.email.value;
+  const comment = form.elements.comment.value;
   const message = document.getElementById('message');
   message.innerHTML = '';
   const re = /@/g;
   if (re.test(String(email).toLowerCase()) === true) {
-    if (email !== email.toLowerCase()) {
+    if (name.length < 1) {
+      form.addaddEventListener('submit', (event) => {
+        event.preventDefault();
+      });
+      message.innerHTML = '* Make sure to fill your name';
+    }
+    else if (email !== email.toLowerCase()) {
       form.addEventListener('submit', (event) => {
         event.preventDefault();
       });
       message.innerHTML = '* Make sure your email is in lower case.';
-    } else {
+    }
+    else if (comment.length < 5) {
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+      });
+      message.innerHTML = '* Text area should have at least 5 characters';
+    }
+    else {
       form.submit();
     }
   }
