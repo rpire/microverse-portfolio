cb1576f18656b38c7d6701471bce797fe69dceae
rpire <rubenpire7@gmail.com>
2021-07-28 10:32:08 -0400

    Apply fix for Linter errors

--- form-validation.js
+++ form-validation.js
@@ -1,19 +1,18 @@
 function validation() {
   const form = document.getElementById('form');
-  const email = form.elements['email'].value;
+  const email = form.elements.email.value;
   const message = document.getElementById('message');
-  message.innerHTML = "";
+  message.innerHTML = '';
   const re = /@/g;
-  if (re.test(String(email).toLowerCase()) == true) {
+  if (re.test(String(email).toLowerCase()) === true) {
     if (email !== email.toLowerCase()) {
-      form.addEventListener('submit', function(event) {
+      form.addEventListener('submit', (event) => {
         event.preventDefault();
       });
-      message.innerHTML = "* Make sure your email is in lower case.";
-      console.log('invalid email!');
+      message.innerHTML = '* Make sure your email is in lower case.';
     } else {
       form.submit();
     }
-    console.log(email);
   }
 }
+validation();
