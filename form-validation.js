8f175e868fc6ce26afcaa80135a26dfd4c5092fe
rpire <rubenpire7@gmail.com>
2021-07-29 11:08:12 -0400

    Apply fix for ESLint errors

--- form-validation.js
+++ form-validation.js
@@ -8,13 +8,13 @@ function validation() {
 
   form.addEventListener('submit', (event) => {
 
-    if (name.length < 1 || name == undefined) {
+    if (name.length < 1 || name === undefined) {
       event.preventDefault();
       message.innerHTML = '* Make sure to fill your name';
-    } else if (email !== email.toLowerCase() || email < 1 || email == undefined) {
+    } else if (email !== email.toLowerCase() || email < 1 || email === undefined) {
       event.preventDefault();
       message.innerHTML = '* Make sure you fill your email and that it is in lower case.';
-    } else if (comment.length < 5 || comment == undefined) {
+    } else if (comment.length < 5 || comment === undefined) {
       event.preventDefault();
       message.innerHTML = '* Text area should have at least 5 characters';
     } else {
@@ -23,4 +23,4 @@ function validation() {
   });
 }
 
-  validation();
+validation();
--- storage.js
+++ storage.js
@@ -1,4 +1,4 @@
-function storeForm() {  
+function storeForm() {
   const form = document.getElementById('form');
   const name = form.name.value;
   const email = form.email.value;
@@ -8,20 +8,21 @@ function storeForm() {
     userEmail: email,
     userComment: comment,
   }
-  
+
   localStorage.setItem('storage', JSON.stringify(myStorage));
 }
 
-window.onload = (event) => {
-  if (!localStorage.getItem('storage')) {
-    console.log('Storage is empty :(');
-  }
-  else {
-    let oldStorage = localStorage.getItem('storage');
-    let newStorage = JSON.parse(oldStorage);
+function refresh() {
+  if (localStorage.getItem('storage')) {
+  const oldStorage = localStorage.getItem('storage');
+    const newStorage = JSON.parse(oldStorage);
     const form = document.getElementById('form');
     form.name.value = newStorage.userName;
     form.email.value = newStorage.userEmail;
     form.comment.value = newStorage.userComment;
   }
 }
+
+window.onload = refresh;
+
+storeForm();
