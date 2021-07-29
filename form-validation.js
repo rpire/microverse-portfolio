e3f2093aa53283d87767568ca11f07a6d41884f1
rpire <rubenpire7@gmail.com>
2021-07-29 10:45:48 -0400

    Fully implement the localStorage function

--- form-validation.js
+++ form-validation.js
@@ -5,15 +5,16 @@ function validation() {
   const comment = form.elements.comment.value;
   const message = document.getElementById('message');
   message.innerHTML = '';
+
   form.addEventListener('submit', (event) => {
-    const form = document.getElementById('form');
-    if (name.length < 1) {
+
+    if (name.length < 1 || name == undefined) {
       event.preventDefault();
       message.innerHTML = '* Make sure to fill your name';
-    } else if (email !== email.toLowerCase()) {
+    } else if (email !== email.toLowerCase() || email < 1 || email == undefined) {
       event.preventDefault();
-      message.innerHTML = '* Make sure your email is in lower case.';
-    } else if (comment.length < 5) {
+      message.innerHTML = '* Make sure you fill your email and that it is in lower case.';
+    } else if (comment.length < 5 || comment == undefined) {
       event.preventDefault();
       message.innerHTML = '* Text area should have at least 5 characters';
     } else {
@@ -22,6 +23,4 @@ function validation() {
   });
 }
 
-if (Math.random() > -99) {
   validation();
-}
--- storage.js
+++ storage.js
@@ -1,10 +1,27 @@
-function storeForm() {  const form = document.getElementById('form');
-  const name = form.nameu.value;
+function storeForm() {  
+  const form = document.getElementById('form');
+  const name = form.name.value;
   const email = form.email.value;
   const comment = form.comment.value;
   let myStorage = {
     userName: name,
     userEmail: email,
     userComment: comment,
-  }  localStorage.setItem('storage', JSON.stringify(myStorage));
+  }
+  
+  localStorage.setItem('storage', JSON.stringify(myStorage));
+}
+
+window.onload = (event) => {
+  if (!localStorage.getItem('storage')) {
+    console.log('Storage is empty :(');
+  }
+  else {
+    let oldStorage = localStorage.getItem('storage');
+    let newStorage = JSON.parse(oldStorage);
+    const form = document.getElementById('form');
+    form.name.value = newStorage.userName;
+    form.email.value = newStorage.userEmail;
+    form.comment.value = newStorage.userComment;
+  }
 }
